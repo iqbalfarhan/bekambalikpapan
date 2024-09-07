e5a081817b7bba95a15797a3b3b6c24f85d89093
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-09-07 19:16:07 +0800

    add deeplink

--- src/constants/Services.ts
+++ src/constants/Services.ts
@@ -2,3 +2,5 @@ export const apiEndpoint = 'https://iqbaltesting.my.id/api';
 export const adminPhone = '081234567890';
 export const androidClientId =
   '1061122686589-7poo7q38vg6daafe5kdlndbuujiq8tl1.apps.googleusercontent.com';
+export const webClientId =
+  '1061122686589-u2s2fju6k1b2l25rukngmjukincgii19.apps.googleusercontent.com';
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Wrapper from '../components/Wrapper';
 import Typo from '../components/Typo';
 import useAuth from '../hooks/useAuth';
@@ -6,44 +6,37 @@ import { inputButtonCardGap } from '../constants/Sizes';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { bgColor } from '../constants/Colors';
-import { Image } from 'react-native';
+import { Alert, Image } from 'react-native';
 import { postLogin } from '../services/userService';
 import * as WebBrowser from 'expo-web-browser';
-import * as AuthSession from 'expo-auth-session';
-
-const LOGIN_URL = 'https://iqbaltesting.my.id/googlelogin';
-const REDIRECT_URI = AuthSession.makeRedirectUri();
+import * as Linking from 'expo-linking';
 
 export default function LoginScreen() {
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await WebBrowser.openAuthSessionAsync(
-        LOGIN_URL,
-        REDIRECT_URI,
-      );
-
-      if (result.type === 'success') {
-        const { url } = result;
-        const token = new URLSearchParams(url.split('?')[1]).get('token');
-
-        if (token) {
-          console.log('Token berhasil didapatkan:', token);
-        } else {
-          throw new Error('Token tidak ditemukan!');
-        }
-      } else {
-        console.log('Login dibatalkan atau gagal');
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   const { login } = useAuth();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [showPass, setShowPass] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
+
+  const handleDeepLink = (event: { url: string }) => {
+    let data = event.url;
+    console.log('URL Deep Link:', data);
+    Alert.alert('Deep Link Diterima', data);
+    WebBrowser.dismissBrowser();
+  };
+
+  useEffect(() => {
+    const subscription = Linking.addEventListener('url', handleDeepLink);
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const openBrowserAndLogin = async () => {
+    await WebBrowser.openBrowserAsync(
+      'https://iqbaltesting.my.id/googleredirect',
+    );
+  };
 
   const handleLogin = async () => {
     if (!email || !password) {
@@ -106,7 +99,7 @@ export default function LoginScreen() {
           imgSource={require('../../assets/googleicon.png')}
           variant='base3'
           label='Login dengan google'
-          onPress={handleGoogleLogin}
+          onPress={openBrowserAndLogin}
         />
       </Wrapper>
     </Wrapper>
